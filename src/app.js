3b7247cc99e44bcdb7cf12eae18129b934b54cee
Himanshu Bhole <himanshubhole50@gmail.com>
2023-10-12 00:08:24 +0530

    Creating the Home Page

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const hbs = require('hbs');
+const { stat } = require('fs');
+const app = express();
+const port = 8000;
+const templete = path.join(__dirname,'../templates/view');
+const partial_path = path.join(__dirname,'../templates/partials');
+const static_path = path.join(__dirname,'../public');
+app.set("view setEngine",'hbs' )
+app.set('views',templete)
+hbs.registerPartials(partial_path)
+app.use(express.static(static_path));
+
+app.get("/",(req,res)=>{
+    res.render('index.hbs')
+})
+
+
+app.listen(port,()=>{
+    console.log('Our Site is Working Fine');
+})
+
