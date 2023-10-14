c97636e2f2cae1fd4b6f5cd083feed2584af0a72
Himanshu Bhole <himanshubhole50@gmail.com>
2023-10-15 00:18:17 +0530

    added navbars menu button

---
+++ public/js/header.js
@@ -0,0 +1,9 @@
+function getMobileheader(){
+    var navbar = document.getElementById('mobile_head');
+    if(navbar.style.display === "none"){
+        navbar.style.display = "flex";
+    }
+    else{
+        navbar.style.display === "none"
+    }
+}
